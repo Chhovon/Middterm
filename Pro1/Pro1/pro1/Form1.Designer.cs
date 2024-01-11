d8be62524eb53388e999904717a198050c51412b
Chhovon <chhovon2@gmail.com>
2024-01-11 08:59:03 +0700

    Exam

---
+++ Pro1/Pro1/pro1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace pro1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtSubtotal = new TextBox();
+            txtDiscountPercent = new TextBox();
+            txtDiscountAmount = new TextBox();
+            txtTotal = new TextBox();
+            btnCaculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 20);
+            label2.TabIndex = 1;
+            label2.Text = "&Subtotal:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 98);
+            label3.Name = "label3";
+            label3.Size = new Size(122, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Discount Percent:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(53, 176);
+            label4.Name = "label4";
+            label4.Size = new Size(127, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Discount Amount:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(53, 259);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Total:";
+            // 
+            // txtSubtotal
+            // 
+            txtSubtotal.Location = new Point(258, 25);
+            txtSubtotal.Name = "txtSubtotal";
+            txtSubtotal.Size = new Size(235, 27);
+            txtSubtotal.TabIndex = 1;
+            // 
+            // txtDiscountPercent
+            // 
+            txtDiscountPercent.Location = new Point(258, 95);
+            txtDiscountPercent.Name = "txtDiscountPercent";
+            txtDiscountPercent.ReadOnly = true;
+            txtDiscountPercent.Size = new Size(235, 27);
+            txtDiscountPercent.TabIndex = 6;
+            txtDiscountPercent.TabStop = false;
+            // 
+            // txtDiscountAmount
+            // 
+            txtDiscountAmount.Location = new Point(258, 173);
+            txtDiscountAmount.Name = "txtDiscountAmount";
+            txtDiscountAmount.ReadOnly = true;
+            txtDiscountAmount.Size = new Size(235, 27);
+            txtDiscountAmount.TabIndex = 7;
+            txtDiscountAmount.TabStop = false;
+            // 
+            // txtTotal
+            // 
+            txtTotal.Location = new Point(258, 256);
+            txtTotal.Name = "txtTotal";
+            txtTotal.ReadOnly = true;
+            txtTotal.Size = new Size(235, 27);
+            txtTotal.TabIndex = 8;
+            txtTotal.TabStop = false;
+            // 
+            // btnCaculate
+            // 
+            btnCaculate.Location = new Point(218, 340);
+            btnCaculate.Name = "btnCaculate";
+            btnCaculate.Size = new Size(113, 36);
+            btnCaculate.TabIndex = 2;
+            btnCaculate.Text = "&Calculate";
+            btnCaculate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(377, 340);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(116, 36);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(545, 436);
+            Controls.Add(btnExit);
+            Controls.Add(btnCaculate);
+            Controls.Add(txtTotal);
+            Controls.Add(txtDiscountAmount);
+            Controls.Add(txtDiscountPercent);
+            Controls.Add(txtSubtotal);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Invoice Total";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtSubtotal;
+        private TextBox txtDiscountPercent;
+        private TextBox txtDiscountAmount;
+        private TextBox txtTotal;
+        private Button btnCaculate;
+        private Button btnExit;
+    }
+}
